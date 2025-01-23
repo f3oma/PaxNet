1ef02a47408a262de54191de4e770418a3481707
Nolan Clark <nolanmclark@gmail.com>
2025-01-22 23:58:29 -0600

    Add announcement template (#30)

---
+++ libs/@shared/src/types/Announcements.ts
@@ -0,0 +1,16 @@
+export type Announcement = {
+    title: string;
+    description: string;
+    backgroundColor: string;
+    textColor: string;
+
+    primaryActionUrl: string;
+    primaryActionText: string;
+    primaryActionButtonColor: string;
+    primaryActionTextColor: string;
+
+    secondaryActionUrl: string;
+    secondaryActionText: string;
+    secondaryActionButtonColor: string;
+    secondaryActionTextColor: string;
+};
